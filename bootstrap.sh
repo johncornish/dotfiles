64b2b8d801f7eafa042d2a21430366b18fd7dab2
Hawkens VonShriek <hawkensvonshriek@gmail.com>
2017-02-18 17:24:27 -0700

    It now actually supports separate profiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,14 +5,10 @@ cd "$(dirname "${BASH_SOURCE}")";
 function do_sync() {
 	#Optionally pass profile directory names to be synced
 	for profile in .core $@; do
-		rsync --exclude ".git/" \
-			--exclude "bootstrap.sh" \
-			--exclude "README.md" \
-			--exclude "LICENSE.md" \
-			-avhP --no-perms $profile/* ~;
+		rsync -avhP --no-perms $profile/* ~;
 	done
 	source ~/.bashrc;
 }
 
-do_sync;
+do_sync $@;
 unset do_sync;
