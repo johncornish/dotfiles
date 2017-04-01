b4813c3e9d133647df95f4f0d68c216eb8ddf2c5
John Cornish <cornish@mymail.mines.edu>
2017-03-31 20:45:36 -0600

    Added default i3 config and fixed bug where hidden files are ignored

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 function do_sync() {
 	#Optionally pass profile directory names to be synced
 	for profile in .core $@; do
-		rsync -avhP --no-perms $profile/* ~;
+		rsync -avhrP --no-perms $profile/ ~;
 	done
 	source ~/.bashrc;
 }
