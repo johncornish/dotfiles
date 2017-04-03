9ea9b31930e2714e9f1c2185aabeedd1294db33c
John Cornish <cornish@mymail.mines.edu>
2017-04-03 15:44:07 -0600

    Automated PPA adding

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,13 +1,9 @@
-sudo apt-get install -y $(cat pkglist)
-
-mkdir deb
-for u in $(cat urllist);
-  #Download only if server has newer version
-  do wget -NP deb/ $u
+for p in $(cat ppalist);
+  do sudo add-apt-repository ppa:$p
 done
-sudo dpkg -i deb/*.deb
 
 sudo apt-get update
-sudo apt-get install -f
+sudo apt-get install -y $(cat pkglist)
+
 sudo apt-get -y upgrade
 sudo apt-get -y autoremove
