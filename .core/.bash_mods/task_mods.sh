88690c353b4e1150f52bc8f7bc904d26b8272ac9
John Cornish <cornish@mymail.mines.edu>
2017-04-19 12:25:20 -0600

    Make tasks from file

--- .core/.bash_mods/task_mods.sh
+++ .core/.bash_mods/task_mods.sh
@@ -1,18 +1,29 @@
 function taskint() {
-    ui="task $@"
-    input=''
-    while [[ $input != 'q' ]]; do
-        clear
-        $ui
-        read input
-        [ "$input" != 'q' ] && task $input
-    done
+  ui="task $@"
+  input=''
+  while [[ $input != 'q' ]]; do
+    clear
+    $ui
+    read input
+    [ "$input" != 'q' ] && task $input
+  done
 }
 
 function taskm() {
+  read input
+  while [[ ! -z $input ]]; do
+    task add $@ $input
     read input
-    while [[ ! -z $input ]]; do
-        task add $@ $input
-        read input
-    done
+  done
+}
+
+function taskfile() {
+  op="task ${@:2}"
+  if [ -f $1 ]; then
+    while IFS= read -r t; do
+      $op $t
+    done < $1
+  else
+    echo "File does not exist"
+  fi
 }
