2ac085bd34530414499d14ff226947140e611d14
John Cornish <cornish@mymail.mines.edu>
2017-04-24 10:09:38 -0600

    .zsh for Z shell and .sh for Bash

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -1,13 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
-cd "$(dirname "${BASH_SOURCE}")";
+callpath=${0:a:h}
 
 function do_sync() {
 	#Optionally pass profile directory names to be synced
 	for profile in .core $@; do
-		rsync -avhrP --no-perms $profile/ ~;
+		rsync -avhrP --no-perms $callpath/$profile/ ~;
 	done
-	source ~/.bashrc;
+	source ~/.zshrc;
 }
 
 do_sync $@;
