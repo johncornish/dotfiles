b83288dd9a5cc8f4e3267095a0238c0a97e0b915
John Cornish <cornish@mymail.mines.edu>
2017-10-06 21:10:08 -0600

    Also sync to S3

--- .core/.oh-my-zsh/plugins/flextime/flextime.plugin.zsh
+++ .core/.oh-my-zsh/plugins/flextime/flextime.plugin.zsh
@@ -1,2 +1,4 @@
 alias fltodo="ftime -f ~/org/tasks.yml todo --lim 5"
 alias flopen="emacs ~/org/tasks.yml"
+alias flpush="aws s3 cp ~/org/tasks.yml s3://org.s3.johncorni.sh"
+alias flpull="aws s3 cp s3://org.s3.johncorni.sh ~/org/tasks.yml"
